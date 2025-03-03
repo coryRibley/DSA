ce7483a3371aa51beb3bb482c3011c487ccb9568
coryRibley <Coryribley@gmail.com>
2025-03-03 14:36:23 -0800

    Final Product for String Match Project

--- LinkedList.java
+++
@@ -1,9 +0,0 @@
-public class LinkedList {
-    Node head;
-
-    public LinkedList(Node head) {
-        this.head = head;
-    }
-}
-
-LinkedList list = new LinkedList(new Node(5));
--- Node.java
+++ Node.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Node {
     int data;
     Node prev;
